048a531d0f6e1ee816d0acde74242baa7ba74200
windi al azmi <windi.azmy@gmail.com>
2024-10-27 01:10:34 +0700

    first commit

---
+++ db/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const dbURL = process.env.DB_URL;
+const db = mongoose.connect(dbURL);
+module.exports = db;
+
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/todos", (req, res) => {
+    res.json({
+        message:"selamat datang"
+    })
+})
+
+
+module.exports = router;
